ba5b4915c862450a1b80af786ae5145336f7f616
ozgurcansakinci <ozgurcansakinci@gmail.com>
2022-08-19 14:24:46 +0200

    Switch to new laptop

--- IEEE9bus_original.jl
+++ IEEE9bus_original.jl
@@ -2,9 +2,9 @@ using HVDCstability,SymEngine
 s = symbols("s")
 @time net = @network begin
     
-    g1 = ac_source(pins = 3, transformation = true)
-    g2 = ac_source(pins = 3, transformation = true)
-    g3 = ac_source(pins = 3, transformation = true)
+    g1 = ac_source(V = 230 * sqrt(2/3), pins = 3, transformation = true)
+    g2 = ac_source(V = 230 * sqrt(2/3), pins = 3, transformation = true)
+    g3 = ac_source(V = 230 * sqrt(2/3), pins = 3, transformation = true)
 
     tl78 = overhead_line(length = 76e3,
             conductors = Conductors(organization = :flat, nᵇ = 3, nˢᵇ = 1, Rᵈᶜ = 0.063, rᶜ = 0.015,  yᵇᶜ = 30,
@@ -42,26 +42,46 @@ s = symbols("s")
             groundwires = Groundwires(nᵍ = 2, Rᵍᵈᶜ = 0.92, rᵍ = 0.0062, Δxᵍ = 6.5, Δyᵍ = 7.5, dᵍˢᵃᵍ   = 10),
             earth_parameters = (1,1,100), transformation = true)
             
+    # Lₘ =, Rₘ =
             
-    t1 = transformer(V₁ᵒ = 2.4e3, V₁ˢ = 51.87, V₂ᵒ = 240, P₁ᵒ = 171.1, P₁ˢ = 642.1,
-            I₁ᵒ = 0.48, I₁ˢ = 20.83, Cₛ = 12e-6, Cₜ = 7e-6, pins = 3, transformation = true)
+    t1 = transformer(n = 16.5/230 , Lₚ = 0.025, Rₚ = 0, Rₛ = 0, Lₛ = 4.8495, Cₜ = 7e-9, Cₛ = 12e-9, pins = 3, transformation = true)
 
-    t2 = transformer(V₁ᵒ = 2.4e3, V₁ˢ = 51.87, V₂ᵒ = 240, P₁ᵒ = 171.1, P₁ˢ = 642.1,
-            I₁ᵒ = 0.48, I₁ˢ = 20.83, Cₛ = 12e-6, Cₜ = 7e-6, pins = 3, transformation = true)
+    t2 = transformer(n = 18/230 ,  Lₚ = 0.0322, Rₚ = 0, Rₛ = 0, Lₛ = 5.2621, Cₜ = 7e-9, Cₛ = 12e-9, pins = 3, transformation = true)
 
-    t3 = transformer(V₁ᵒ = 2.4e3, V₁ˢ = 51.87, V₂ᵒ = 240, P₁ᵒ = 171.1, P₁ˢ = 642.1,
-            I₁ᵒ = 0.48, I₁ˢ = 20.83, Cₛ = 12e-6, Cₜ = 7e-6, pins = 3, transformation = true)
+    t3 = transformer(n = 13.8/230 , Lₚ =0.0178, Rₚ = 0, Rₛ = 0, Lₛ = 4.9337, Cₜ = 7e-9, Cₛ = 12e-9, pins = 3, transformation = true)
 
-    l5 = impedance(z = 423.2+1/3.36s/s, pins = 3, transformation = true) # Nominal P 125 MW, nominal Q 50 MVar
-    l6 = impedance(z = 587.78+1/5.61s/s, pins = 3, transformation = true) # Nominal P 125 MW, nominal Q 50 MVar
-    l8 = impedance(z = 529+1/4.81s/s, pins = 3, transformation = true) # Nominal P 125 MW, nominal Q 50 MVar
+#     l5 = impedance(z = 423.2 + 3.36s, pins = 3, transformation = true) # Nominal P 125 MW, nominal Q 50 MVar
+#     l6 = impedance(z = 587.78 + 5.61s, pins = 3, transformation = true) # Nominal P 125 MW, nominal Q 50 MVar
+#     l8 = impedance(z = 529 + 4.81s, pins = 3, transformation = true) # Nominal P 125 MW, nominal Q 50 MVar
 
+    l5 = impedance(z = 1269.6 + 10.1s, pins = 3, transformation = true) # Nominal P 41.6667 MW, nominal Q 16.66667 MVar
+    l6 = impedance(z = 1763.3 + 16.84s, pins = 3, transformation = true) # Nominal P 30 MW, nominal Q 10 MVar
+    l8 = impedance(z = 1587.16 + 14.43s, pins = 3, transformation = true) # Nominal P 33.33 MW, nominal Q 11.6667 MVar
 
-    g1[1.1] == gnd1d
-    g1[1.2] == gnd1q
+    g1[1.1] == gndd
+    g1[1.2] == gndq
 
     g1[2.1] == t1[1.1] == Bus1d
     g1[2.2] == t1[1.2] == Bus1q
+
+#     c1 = mmc(Vᵈᶜ = 640, Vₘ = 230*sqrt(2/3),
+#                 P_max = 1500, P_min = -1500, P = 100, Q = 0, Q_max = 500, Q_min = -500, P_dc = -100,
+#                 occ = PI_control(ζ = 0.7, bandwidth = 1000),
+#                 ccc = PI_control(ζ = 0.7, bandwidth = 300),
+#                 power = PI_control(Kₚ = 2.0020e-07, Kᵢ = 1.0010e-04),
+#                 energy = PI_control(Kₚ = 120, Kᵢ = 400, ref=[1*3072e4]),
+#                 zcc = PI_control(ζ = 0.7, bandwidth = 300),
+#                 timeDelay=150e-6, padeOrderNum=3, padeOrderDen=3
+#                 )
+
+#     dc = dc_source(V = 640)
+
+#     dc[1.1] == gndd
+
+#     c1[1.1] == dc[2.1]
+
+#     c1[2.1] == t1[1.1] == Bus1d
+#     c1[2.2] == t1[1.2] == Bus1q
 
     t1[2.1] == tl64[1.1] == tl54[1.1] == Bus4d
     t1[2.2] == tl64[1.2] == tl54[1.2] == Bus4q
@@ -69,46 +89,46 @@ s = symbols("s")
     tl64[2.1] == l6[1.1] == tl96[1.1] == Bus6d
     tl64[2.2] == l6[1.2] == tl96[1.2] == Bus6q
 
-    l6[2.1] == gnd6d
-    l6[2.2] == gnd6q
+    l6[2.1] == gndd
+    l6[2.2] == gndq
 
     tl54[2.1] == l5[1.1] == tl75[1.1] == Bus5d
     tl54[2.2] == l5[1.2] == tl75[1.2] == Bus5q
 
-    l5[2.1] == gnd5d
-    l5[2.2] == gnd5q
+    l5[2.1] == gndd
+    l5[2.2] == gndq
 
-    tl96[2.1] == tl89[1.1] == t3[1.1] == Bus9d
-    tl96[2.2] == tl89[1.2] == t3[1.2] == Bus9q
+    tl96[2.1] == tl89[1.1] == t3[2.1] == Bus9d
+    tl96[2.2] == tl89[1.2] == t3[2.2] == Bus9q
 
-    tl75[2.1] == tl78[1.1] == t2[1.1] == Bus7d
-    tl75[2.2] == tl78[1.2] == t2[1.2] == Bus7q
+    tl75[2.1] == tl78[1.1] == t2[2.1] == Bus7d
+    tl75[2.2] == tl78[1.2] == t2[2.2] == Bus7q
 
     tl78[2.1] == tl89[2.1] == l8[1.1] == Bus8d
     tl78[2.2] == tl89[2.2] == l8[1.2] == Bus8q
 
-    l8[2.1] == gnd8d
-    l8[2.2] == gnd8q
+    l8[2.1] == gndd
+    l8[2.2] == gndq
 
-    t2[2.1] == g2[1.1] == Bus2d
-    t2[2.2] == g2[1.2] == Bus2q
+    t2[1.1] == g2[1.1] == Bus2d
+    t2[1.2] == g2[1.2] == Bus2q
 
-    g2[2.1] == gnd2d
-    g2[2.2] == gnd2q
+    g2[2.1] == gndd
+    g2[2.2] == gndq
 
-    t3[2.1] == g3[1.1] == Bus3d
-    t3[2.2] == g3[1.2] == Bus3q
+    t3[1.1] == g3[1.1] == Bus3d
+    t3[1.2] == g3[1.2] == Bus3q
 
-    g3[2.1] == gnd3d
-    g3[2.2] == gnd3q
+    g3[2.1] == gndd
+    g3[2.2] == gndq
 
 end
 
 #@time imp_ac, omega_ac = determine_impedance(net, elim_elements=[:gen1], input_pins=Any[:TestNode1,:TestNode2,:TestNode3], 
 #output_pins=Any[:gnd,:gnd,:gnd], omega_range = (0,4,1000))
 
-@time imp_ac, omega_ac = determine_impedance(net, elim_elements=[:g1], input_pins=Any[:Bus1d,:Bus1q], 
-output_pins=Any[:gnd3d,:gnd3q], omega_range = (0,4,1000))
+@time imp_ac, omega_ac = determine_impedance(net, elim_elements=[:g2], input_pins=Any[:Bus2d,:Bus2q], 
+output_pins=Any[:gndd,:gndq], omega_range = (0,4,1000))
 
 # imp_ac_d = []
 # imp_ac_q = []
@@ -119,4 +139,7 @@ output_pins=Any[:gnd3d,:gnd3q], omega_range = (0,4,1000))
 # end
 p = bode(imp_ac, omega = omega_ac)
 
+# save_plot(p,string("files/", "from_Bus2_to_gnd_G3_withConverter", ".pdf"))
 save_plot(p)
+
+
--- test/tests/gridDynamics_test.jl
+++ test/tests/gridDynamics_test.jl
@@ -12,19 +12,19 @@ s = symbols("s")
     # gen1[2.3] == z[1.3] == TestNode3
     # z[2.1] == z[2.2] == z[2.3] == gnd
 
-    gen1[2.1] == gnd
-    gen1[2.2] == gnd
+    gen1[2.1] == gnd1d
+    gen1[2.2] == gnd2q
     gen1[1.1] == z[1.1] == TestNode1
     gen1[1.2] == z[1.2] == TestNode2
-    z[2.1] == gnd1
-    z[2.2] == gnd2
+    z[2.1] == gnd1dd
+    z[2.2] == gnd2qq
 end
 
 #@time imp_ac, omega_ac = determine_impedance(net, elim_elements=[:gen1], input_pins=Any[:TestNode1,:TestNode2,:TestNode3], 
 #output_pins=Any[:gnd,:gnd,:gnd], omega_range = (0,4,1000))
 
 @time imp_ac, omega_ac = determine_impedance(net, elim_elements=[:gen1], input_pins=Any[:TestNode1,:TestNode2], 
-output_pins=Any[:gnd1,:gnd2], omega_range = (0,4,1000))
+output_pins=Any[:gnd1dd,:gnd2qq], omega_range = (0,4,1000))
 
 # imp_ac_d = []
 # imp_ac_q = []
@@ -33,6 +33,6 @@ output_pins=Any[:gnd1,:gnd2], omega_range = (0,4,1000))
 #     push!(imp_ac_d,imp_ac[n,1][1])
 #     push!(imp_ac_q,imp_ac[n,1][2])
 # end
-#p = bode(imp_ac, omega = omega_ac)
+p = bode(imp_ac, omega = omega_ac)
 
-#save_plot(p)
+save_plot(p)
