e332f4a2397ed2f0dcfaf4064b4bd5fb4e00fc4b
u0167553 <robbe.vandereeckt@kuleuven.be>
2024-12-23 16:00:49 +0100

    try example amauri make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -28,8 +28,10 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
+GITLAB_TOKEN = ENV["GITLAB_TOKEN"]
+
 deploydocs(
-    repo = "gitlab.kuleuven.be/electa/controlgroup/hvdcstability.jl.git",
+    repo = "u0167553:$GITLAB_TOKEN@gitlab.kuleuven.be/electa/controlgroup/hvdcstability.jl.git",
     devbranch="main",
     branch="gl-pages",
     deploy_config = Documenter.GitLab()
