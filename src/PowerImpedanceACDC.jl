cccb512d82da1304e7bcd26d02bd8dfe9f658097
u0158626 <jan.kircheis@kuleuven.be>
2025-04-22 10:21:54 +0200

    Update version number in Project.toml and remove unused PGFPlotsX dependency

--- src/PowerImpedanceACDC.jl
+++ src/PowerImpedanceACDC.jl
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License along with thi
     using ControlSystemsBase, RobustAndOptimalControl
 
     # Plotting
-    using PGFPlotsX
     using Plots
 
     # Symbolic and numerical calculations
