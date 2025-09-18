d31a8fd16cbba6a103ded60fab42b8ee144b1df6
u0158626 <jan.kircheis@kuleuven.be>
2025-09-18 15:14:39 -0600

    Fix: Uncomment JLD2 usage and power flow test inclusion in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
-#using JLD2
+using JLD2
 using PowerImpedanceACDC
 using Test
 
 
 @testset "PowerImpedanceACDC" begin
         
-        #include("power_flow_test.jl")
+        include("power_flow_test.jl")
         include("imp_test.jl")
         include("adm_MMC_test.jl")
 
